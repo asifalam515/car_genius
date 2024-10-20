775445831ebbe5c37ee0c79aaf79b91a48f0d50a
asifalam515 <asibulalam515@gmail.com>
2024-10-20 10:40:08 +0600

    navbar started

--- car_genius_client/src/Routes/Routes.jsx
+++ car_genius_client/src/Routes/Routes.jsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
-    children: [{ path: "/" }],
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
   },
 ]);
 export default router;
